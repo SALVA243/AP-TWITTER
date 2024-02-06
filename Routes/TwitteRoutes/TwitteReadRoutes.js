e3a215ffbe413c0a91687066d0350397a1d2efc8
SALVA LUPETA <salvakanzeka1@gmail.com>
2024-02-07 01:34:56 +0200

    Sprint 1 Terminé 2

--- Routes/TwitteRoutes/TwitteReadRoutes.js
+++ Routes/TwitteRoutes/TwitteReadRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router();
 
 router.get(('/'), (req, res) => {
-    res.json(console.log("Voici les Twittes"));
+    res.json(console.log("Affichage des Twittes"));
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-// app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded());
 
 app.use("/Posts", require("./Routes/TwitteRoutes/TwitteReadRoutes"));
 // app.use("/Posts", require("./Routes/TwitteRoutes/TwitteReadRoutes"));
